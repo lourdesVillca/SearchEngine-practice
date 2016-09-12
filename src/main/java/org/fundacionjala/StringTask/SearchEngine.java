460af632e801ec42c9a1cb1dc0838962b048a460
Lourdes Villca Lazaro <LourdesVillca@Fundacion-jala.local>
2016-09-12 09:59:39 -0400

    fix comments

--- src/main/java/org/fundacionjala/StringTask/SearchEngine.java
+++ src/main/java/org/fundacionjala/StringTask/SearchEngine.java
@@ -1,17 +1,27 @@
-package org.fundacionjala.StringTask;
+package org.fundacionjala.stringtask;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created by LourdesVillca on 9/2/2016.
+ * Class that make a search in a String
  */
 public class SearchEngine {
-    
+
+    private SearchEngine() {
+
+    }
+
+    /**
+     * Method that make the search process
+     * @param needle the string to be searched
+     * @param haystack String where the search process is executed
+     * @return the start position if the word is found if not it return -1
+     */
     public static int find(String needle, String haystack) {
-            String regex = "\\Q" + needle.replaceAll("_", "\\\\E.\\\\Q") + "\\E";
-            Matcher matcher = Pattern.compile(regex).matcher(haystack);
-            return matcher.find() ? matcher.start() : -1;
+        final String regex = "\\Q" + needle.replaceAll("_", "\\\\E.\\\\Q") + "\\E";
+        Matcher matcher = Pattern.compile(regex).matcher(haystack);
+        return matcher.find() ? matcher.start() : -1;
     }
 
 }
--- src/test/java/org/fundacionjala/StringTask/WildsTest.java
+++ src/test/java/org/fundacionjala/StringTask/WildsTest.java
@@ -1,22 +1,26 @@
-package org.fundacionjala.StringTask;
+package org.fundacionjala.stringtask;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by LourdesVillca on 9/2/2016.
+ * Test for {@link SearchEngine}
  */
 public class WildsTest {
-    String haystack = "Once upon a midnight dreary, while I pondered, weak and weary";
+
+    private static final String HAYSTACK = "Once upon a midnight dreary, while I pondered, weak and weary";
+
     @Test
-    public void normalSearchTest(){
-        assertEquals(0,SearchEngine.find("Once", haystack));
-        assertEquals(12, SearchEngine.find("midnight", haystack));
-        assertEquals(-1, SearchEngine.find("codewars", haystack));
+    public void normalSearchTest() {
+        assertEquals(0, SearchEngine.find("Once", HAYSTACK));
+        assertEquals(12, SearchEngine.find("midnight", HAYSTACK));
+        assertEquals(-1, SearchEngine.find("codewars", HAYSTACK));
     }
+
     @Test
-    public void wildSearchTest(){
-        assertEquals(5, SearchEngine.find("_po_", haystack));
-        assertEquals(12, SearchEngine.find("___night", haystack));
+    public void wildSearchTest() {
+        assertEquals(5, SearchEngine.find("_po_", HAYSTACK));
+        assertEquals(12, SearchEngine.find("___night", HAYSTACK));
     }
 }
